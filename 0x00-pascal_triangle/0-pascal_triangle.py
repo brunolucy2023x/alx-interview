f3d45fbfb2fe2c4aea0da05bacae66e0b586a1f4
brunox327 <lucyb5147@gmail.com>
2024-10-03 05:46:39 +0000

    pascal four

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
 """
 0-pascal_triangle
-Author: Bruno Okoth
 """
+
 
 def pascal_triangle(n):
     """
-    Returns a list of lists representing the Pascal Triangle of height n.
-    Returns an empty list if n <= 0.
+    Returns a list of integers
+    representing the Pascal Triangle of n
+    returns empty list if n <= 0
     """
-    k = []  # Initialize an empty list to hold the rows of Pascal's Triangle
-
-    # Check if the input height n is less than or equal to 0
+    k = []
     if n <= 0:
-        return k  # Return the empty list if n is not positive
-
-    k = [[1]]  # The first row of Pascal's Triangle is always [1]
-
-    # Loop through each row from 1 to n-1
+        return k
+    k = [[1]]
     for i in range(1, n):
-        temp = [1]  # Start each row with a 1
-        # Loop through the previous row to calculate the values in the current row
+        temp = [1]
         for j in range(len(k[i - 1]) - 1):
-            curr = k[i - 1]  # Get the previous row
-            # Calculate the value as the sum of two values from the previous row
+            curr = k[i - 1]
             temp.append(k[i - 1][j] + k[i - 1][j + 1])
-        temp.append(1)  # End each row with a 1
-        k.append(temp)  # Add the current row to the triangle
-
-    return k  # Return the complete Pascal's Triangle
-
+        temp.append(1)
+        k.append(temp)
+    return k
