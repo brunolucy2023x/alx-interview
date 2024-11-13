9831e53ac78b22f2733d02df66eda4dc15b9f532
brunolucy2023x <144634638+brunolucy2023x@users.noreply.github.com>
2024-11-13 13:32:51 +0300

    three

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,50 +1,33 @@
 #!/usr/bin/node
-const request = require('request');
 
-if (process.argv.length !== 3) {
-  console.error('Usage: ./0-starwars_characters.js <Movie_ID>');
-  process.exit(1);
+const request = require('request');
+const movieUrl = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+
+function fetchCharacterData (characterUrl) {
+  return new Promise((resolve, reject) => {
+    request(characterUrl, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const characterData = JSON.parse(body);
+        resolve(characterData.name);
+      }
+    });
+  });
 }
 
-const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+function fetchAndPrintCharacterNames (movieUrl) {
+  request(movieUrl, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const movieData = JSON.parse(body);
+      const characterUrls = movieData.characters;
 
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    process.exit(1);
-  }
+      const promises = characterUrls.map(fetchCharacterData);
 
-  if (response.statusCode !== 200) {
-    console.error('Failed to retrieve movie information. Status Code:', response.statusCode);
-    process.exit(1);
-  }
-
-  const movieData = JSON.parse(body);
-  const characters = movieData.characters;
-  async function getCharacterData (characterUrl) {
-    return new Promise((resolve, reject) => {
-      request(characterUrl, (error, response, body) => {
-        if (error) {
-          reject(error);
-        } else if (response.statusCode !== 200) {
-          reject(`Failed to retrieve character information. Status Code: ${response.statusCode}`);
-        } else {
-          resolve(JSON.parse(body));
-        }
-      });
-    });
-  }
-
-  (async function printCharacters () {
-    for (const characterUrl of characters) {
-      try {
-        const characterData = await getCharacterData(characterUrl);
-        console.log(characterData.name);
-      } catch (error) {
-        console.error('Error:', error);
-        process.exit(1);
-      }
+      Promise.all(promises)
+        .then(characterNames => {
+          characterNames.forEach(name => console.log(name));
+        });
     }
-  })();
-});
+  });
+}
+
+fetchAndPrintCharacterNames(movieUrl);
